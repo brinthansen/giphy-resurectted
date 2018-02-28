fab79cfcc064cf924bd535cbf92e59c37f3e83e9
brint hansen <hansenb@redeyeusa.com>
2018-02-28 16:02:00 -0500

    final commit

--- gifApp.js
+++ gifApp.js
@@ -1,13 +1,21 @@
     	$(document).ready(function() {
 
-    	var gifTopics = [];
+
+
+     
+      var gifTopics = [];
+      console.log(gifTopics)
+      
+
+      
+      
       
 
 
-	function myFunction(){
+  function getGif(){
 
-   		var x = $(this).data("search");
-      var offset = Math.floor((Math.random() * 100) + 1);
+      var x = $(this).data("search");
+      var offset = Math.floor((Math.random() * 300) + 1);
 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x 
          + "&offset=" + offset + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -20,48 +28,51 @@
          
 
           .done(function(response) {
-          $(".gif-view").empty()
-        	var results = response.data;
-        	console.log(results);
-        	for (var i = 0; i <results.length; i++){
+          $(".column").empty()
+          var results = response.data;
+          
+          for (var i = 0; i <results.length; i++){
 
             
 
-        	var gifDiv = $("<div class=gifLook>" );
-        	var rating = results[i].rating;
-        	var animatedSrc = results[i].images.downsized_large.url;
-        	var staticSrc = results[i].images.downsized_still.url;
-        	var gifImage = $("<img>");
-        	var p = $("<p>");
+          var gifDiv = $("<div class=giff>" + i );
+          var rating = results[i].rating;
+          var animatedSrc = results[i].images.downsized_large.url;
+          var staticSrc = results[i].images.downsized_still.url;
+          var gifImage = $("<img>");
+          var p = $("<p>");
 
-          gifImage.attr("id","gifs" + i);
+          gifImage.attr("id","gifs");
           gifDiv.attr("id", "gif" + i );
-        	gifImage.attr("src", staticSrc);
-        	gifImage.addClass("gifGiphy");
-        	gifImage.attr("data-still", staticSrc);
-        	gifImage.attr("data-animate", animatedSrc);
-        	gifDiv.append(p);
-        	gifDiv.append(gifImage);
+          gifImage.attr("src", staticSrc);
+          gifImage.addClass("gifGiphy");
+          gifImage.attr("data-still", staticSrc);
+          gifImage.attr("data-animate", animatedSrc);
+          gifDiv.append(p);
+          gifDiv.append(gifImage);
 
 
-          $(".gifLook").width( "200px" );
-          $(".gifLook").height( "130px" );
-        	$(".gif-view").prepend(gifDiv);
-          $(".gif-view").val(''); 
+          $(".gifLook").width( "400px" );
+          $(".gifLook").height( "400px" );
+          $(".column").prepend(gifDiv);
+          $(".column").val(''); 
 
                  
-}       	       
-	});
+}                
+  });
 }
 
 
 
 
-	$("#addGif").on("click", function(event) {
+  $("#addGif").on("click", function(event) {
         event.preventDefault();
         var newGif = $("#gifInput").val().trim();
         gifTopics.push(newGif);
+        var gifTopics2 = gifTopics.splice(0,4);
+        
         console.log(gifTopics);
+
         $("#gifInput").val('');
         displayButtons();
       
@@ -69,7 +80,7 @@
       });
 
 
-	function displayButtons() {
+  function displayButtons() {
     $("#myButtons").empty();
     for (var i = 0; i < gifTopics.length; i++) {
       var a = $('<button class="btn btn-primary">');
@@ -83,28 +94,31 @@
 
   displayButtons();
 
-  $(document).on("click", "#gif", myFunction);
+ 
+
+  $(document).on("click", "#gif", getGif);
 
   
-  $(document).on("click", ".gifGiphy", pausePlayGifs);
+  $(document).on("click", "#gifs", pausePlayGifs);
 
   
 
  
   function pausePlayGifs() {
-  	 var state = $(this).attr("data-state");
+     var state = $(this).attr("data-state");
       if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
-      } else {
+      } 
+      else {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
   }
 }
 
+console.log(gifTopics)
+
 
 
 
 });
-
-  
